4444d98423c3a4b3ca26df19077876a9d4437192
Yuliia <juliadenisyuk2006@gmail.com>
2024-02-13 21:12:39 +0200

    yulla utils

--- utils.py
+++ utils.py
@@ -1,3 +1,10 @@
 def factorial(n):
     return 1 if n <= 1 else n * factorial(n - 1)
 
+def ncd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+num1 = int(input())
+num2 = int(input())
